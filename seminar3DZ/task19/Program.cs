7ed5e5775379d4a81f0898520a4c784166be994a
rory <nextemus@gmail.com>
2023-03-09 20:21:08 +0300

    seminar 3 homework completed

---
+++ seminar3DZ/task19/Program.cs
@@ -0,0 +1,28 @@
+﻿
+// Задание 19
+Console.Write("Введите 5-ти значное число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int num0 = number / 10000;
+int num1 = number / 1000 % 10;
+int num2 = number / 100 % 10;
+int num3 = number / 10 % 10;
+int num4 = number % 10;
+
+
+if (number >= 10000 && number < 100000)
+{
+    if (num0 == num4 && num1 == num3)
+    {
+        Console.WriteLine($"Вы ввели, {number} ваше число палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"Вы ввели, {number} ваше число не палиндром, УВЫ");
+    }
+}
+else
+{
+    Console.WriteLine($"Вы ввели {number} , это не 5-ти значное число, УВЫ");
+}
---
+++ seminar3DZ/task21/Program.cs
@@ -0,0 +1,25 @@
+﻿ // Задание 21
+ int getInt(){
+
+ return Convert.ToInt32(Console.ReadLine());
+
+ }
+
+ Console.WriteLine("Введите 3 координаты точек A, B :");
+
+ int x1 = getInt();
+ int y1 = getInt();
+ int z1 = getInt();
+ int x2 = getInt();
+ int y2 = getInt();
+int z2 = getInt();
+
+
+
+
+ double d = Math.Round(Math.Sqrt(Math.Pow((x2-x1), 2) + 
+                    Math.Pow((y2-y1), 2) + 
+                    Math.Pow((z2-z1), 2)),2);
+
+
+Console.WriteLine(d);
